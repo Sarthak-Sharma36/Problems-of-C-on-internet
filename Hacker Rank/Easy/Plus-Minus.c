e81a4e43adc6d84c1b613b78eb42a0bc009af6fb
Sarthak-Sharma36 <76254143+Sarthak-Sharma36@users.noreply.github.com>
2021-01-16 18:36:17 +0530

    Add files via upload

---
+++ Hacker Rank/Easy/Plus-Minus.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ int  userinput = 0,element = 0,p = 0,n=0,z=0;
+
+	scanf("%d",&userinput);
+	float  arr[userinput];
+	for(int i = 0;i<userinput;i++)
+	{
+		scanf("%f",&arr[element]);
+		if(arr[element]>0)
+        p++;
+    else if(arr[element]<0)
+    	n++;
+    else
+    	z++;
+	}
+	printf("%f\n%f\n%f",p/userinput,n/userinput,z/userinput);
+
+}
